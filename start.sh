58b6b553f92302a45ff7d9a339072b656403ae57
oerday <orhan.erday@team.bumble.com>
2025-10-07 10:54:02 +0300

    cleanup

--- start.sh
+++ start.sh
@@ -4,4 +4,4 @@
 celery -A src.modules.workers.celery.celery_app worker --loglevel=info &
 
 # Start FastAPI app using Gunicorn with UvicornWorker
-exec gunicorn src.main:app -k uvicorn.workers.UvicornWorker --bind 0.0.0.0:8000 --workers 6
+exec gunicorn src.main:app -k uvicorn.workers.UvicornWorker --bind 0.0.0.0:8000 --workers 9
